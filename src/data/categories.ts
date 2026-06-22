1d4b7ae28decf1132089d5593661fee5899f35e7
fishcpy <fishcpy@foxmail.com>
2026-06-22 23:06:11 +0800

    fix(data): 补全categories里displayUrl的完整域名

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -5,19 +5,19 @@ export const categories: CategoryInfo[] = [
     id: 'mc',
     name: '我的世界',
     apiUrl: 'https://api.fis.ink/img/pic/mc.json',
-    displayUrl: 'pic.fis.ink/mc',
+    displayUrl: 'https://pic.fis.ink/mc',
   },
   {
     id: 'aqtw',
     name: '暗区突围',
     apiUrl: 'https://api.fis.ink/img/pic/aqtw.json',
-    displayUrl: 'pic.fis.ink/aqtw',
+    displayUrl: 'https://pic.fis.ink/aqtw',
   },
   {
     id: 'end',
     name: '终末地',
     apiUrl: 'https://api.fis.ink/img/pic/end.json',
-    displayUrl: 'pic.fis.ink/end',
+    displayUrl: 'https://pic.fis.ink/end',
   },
 ]
 
